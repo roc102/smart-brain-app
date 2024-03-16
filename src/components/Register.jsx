e373e4fa37832a10735583bae0b1e5eb7b04d728
roc102 <mdfahadrahman1969@gmail.com>
2024-03-16 22:23:33 +0530

    setup

---
+++ src/components/Register.jsx
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "tachyons";
+
+const Register = ({ loadUser, onRouteChange }) => {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+    name: "",
+  });
+
+  const onNameChange = (event) => {
+    setState({ ...state, name: event.target.value });
+  };
+
+  const onEmailChange = (event) => {
+    setState({ ...state, email: event.target.value });
+  };
+
+  const onPasswordChange = (event) => {
+    setState({ ...state, password: event.target.value });
+  };
+
+  const onSubmitRegister = () => {
+    const { name, email, password } = state;
+
+    if (!name || !email || !password) {
+      toast.error("Please fill in all fields");
+      return;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      toast.error("Please enter a valid email address");
+      return;
+    }
+
+    if (password.length < 6) {
+      toast.error("Password should be at least 6 characters long");
+      return;
+    }
+  };
+
+  return (
+    <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <main className="pa4 black-80">
+        <div className="measure">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f1 fw6 ph0 mh0">Register</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="name">
+                Name
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="text"
+                name="name"
+                id="name"
+                onChange={onNameChange}
+              />
+            </div>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                Email
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="email"
+                name="email-address"
+                id="email-address"
+                onChange={onEmailChange}
+              />
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="password"
+                name="password"
+                id="password"
+                onChange={onPasswordChange}
+              />
+            </div>
+          </fieldset>
+          <div className="">
+            <input
+              onClick={onSubmitRegister}
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+              type="submit"
+              value="Register"
+            />
+          </div>
+        </div>
+      </main>
+      <ToastContainer />
+    </article>
+  );
+};
+
+export default Register;
